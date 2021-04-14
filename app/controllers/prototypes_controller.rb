1ac64e28f959a7439a103707b97b47a418194f0b
yuki maegaki <on96.maegaki.yuki@gmail.com>
2021-04-14 10:44:32 +0900

    編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:edit, :destroy,]
 
   def index
     @prototypes = Prototype.all
@@ -48,16 +47,17 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 
 end
